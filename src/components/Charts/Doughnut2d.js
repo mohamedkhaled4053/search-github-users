70e3c6018713e81fe06a735268b159249042c9ce
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-04 23:10:26 +0200

    finish Doughnut2d.js

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
+import ReactFC from 'react-fusioncharts';
 
-export default Doughnut2d;
+import FusionCharts from 'fusioncharts';
+
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+function Pie2D({data}) {
+
+
+  const chartConfigs = {
+    type: 'doughnut2d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: 'star per language',
+        theme: 'fusion',
+        decimals: '0',
+        pieRadius: '45%',
+        showPercentValues: "0",
+        plottooltext: '$label, $percentValue',
+      },
+      
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+}
+
+export default Pie2D;
