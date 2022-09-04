2c6b9483fde15218a825d2ecf9c8279ed367b4ce
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-04 17:15:27 +0200

    set up Repos.js

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,7 +3,16 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  return (
+    <section>
+      <Wrapper>
+        <Pie3D />
+        <Column3D />
+        <Doughnut2D />
+        <Bar3D />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
