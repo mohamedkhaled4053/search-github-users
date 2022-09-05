726589fb00c1640ca889d1cd7a3c8eead8a36178
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-05 12:08:43 +0200

    fetch data from api instead of the mockData

--- src/context/context.js
+++ src/context/context.js
@@ -9,12 +9,44 @@ const rootUrl = 'https://api.github.com';
 let GithubContext = React.createContext();
 
 function GithubProvider({ children }) {
-  let [user, setUser] = useState(mockUser);
-  let [repos, setRepos] = useState(mockRepos);
-  let [followers, setFollowers] = useState(mockFollowers);
+  let [user, setUser] = useState({});
+  let [repos, setRepos] = useState([]);
+  let [followers, setFollowers] = useState([]);
+  let [userName, setUserName] = useState('john-smilga');
+  let [loading, setLoading] = useState(true);
+
+  function fetchData() {
+    setLoading(true)
+
+    // prepare urls
+    let userUrl = `${rootUrl}/users/${userName}`;
+    let followersUrl = userUrl + '/followers';
+    let reposUrl = userUrl + '/repos?per_page=100';
+
+    // setup fetch
+    let fetchUser = fetch(userUrl).then((res) => res.json());
+    let fetchFollowers = fetch(followersUrl).then((res) => res.json());
+    let fetchRepos = fetch(reposUrl).then((res) => res.json());
+
+    // display data only when all settled
+    Promise.allSettled([fetchUser, fetchFollowers, fetchRepos]).then(
+      (res) => {
+        setUser(res[0].value)
+        setFollowers(res[1].value)
+        setRepos(res[2].value)
+        setLoading(false)
+      }
+    );
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <GithubContext.Provider value={{ user, repos, followers }}>
+    <GithubContext.Provider
+      value={{ user, repos, followers, userName, setUserName, loading }}
+    >
       {children}
     </GithubContext.Provider>
   );
