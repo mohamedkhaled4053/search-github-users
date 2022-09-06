8397d82406db99fc6f7b62e0d93b4fc1ec5913b5
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-06 17:13:29 +0200

    prepare for Auth0

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
-  </>,
+  <React.StrictMode>
+    <Auth0Provider
+        domain={process.env.REACT_APP_DOMAIN}
+        clientId={process.env.REACT_APP_CLIENID}
+        redirectUri={window.location.origin}
+        >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
